24a4eae7727e887db18b671916bb40f744f8e59e
Orlando Davila <odavila0308@gmail.com>
2020-08-18 16:54:29 -0400

    complete

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import ProtectedRoute from './components/ProjectedRoute';
+import FriendsList from './components/FriendsList';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <Link to="/login">Login</Link>
+         <Link to="/protected">Protected Page</Link>
+
+          <Switch>
+            <ProtectedRoute exact path ='/protected' component= {FriendsList}/>
+            <Route path='/login' component ={Login} />
+            <Route component={Login} />
+          </Switch>
       </header>
     </div>
+    </Router>
   );
-}
-
-export default App;
+  }
+  export default App;
